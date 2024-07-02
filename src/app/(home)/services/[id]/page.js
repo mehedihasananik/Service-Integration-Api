c5a4a6fca78fe6cbdb278b9fab453e2340204a58
mehedihasananikoffcial <marshallanique@gmail.com>
2024-07-02 12:43:02 +0600

    updated server side rendering

--- src/app/(home)/services/[id]/page.js
+++ src/app/(home)/services/[id]/page.js
@@ -1,33 +1,11 @@
 import ServiceDetails from "@/Components/PagesComponents/ServiceDetails/ServiceDetails";
-import UserLoading from "@/Components/Utilites/UserLoading/UserLoading";
-import {
-  singeServiceDetails,
-  singleService_package,
-  singleSliderPageDetails,
-} from "@/config/apis";
 import { Suspense } from "react";
 
-const SinglePage = async ({ params }) => {
-  // console.log(params.id);
-  // Fetch data for the page
-  const service = await fetch(`${singeServiceDetails}/${params?.id}`).then(
-    (res) => res?.json()
-  );
-  const sliders = await fetch(`${singleSliderPageDetails}/${params?.id}`).then(
-    (res) => res?.json()
-  );
-  const packages = await fetch(`${singleService_package}/${params?.id}`).then(
-    (res) => res?.json()
-  );
-  // console.log(`${singleSliderPageDetails}/${params?.id}`);
+const SinglePage = ({ params }) => {
   return (
     <>
-      <Suspense fallback={<UserLoading />}>
-        <ServiceDetails
-          service={service}
-          sliders={sliders}
-          packages={packages}
-        />
+      <Suspense>
+        <ServiceDetails id={params?.id} />
       </Suspense>
     </>
   );
