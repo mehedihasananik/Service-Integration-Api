d9a21a3f053dc6e57fa0a3d27d554cdd36703f8a
mehedihasananikoffcial <marshallanique@gmail.com>
2024-07-07 18:17:45 +0600

    updated slider

--- src/Components/Utilites/PortfolioHomeItems/PortfolioHomeItems.jsx
+++ src/Components/Utilites/PortfolioHomeItems/PortfolioHomeItems.jsx
@@ -97,62 +97,44 @@ const PortfolioHomeItems = ({ portfolios, services }) => {
                     key={portfolio.id}
                     href={`/portfolio/${portfolio.slug}`}
                   >
-                    <div className="group border border-[#CBD5E1] rounded-[10px]">
-                      <div className="portfolio-bgHover cursor-pointer xxl:space-x-4 4xl:space-x-0 flex flex-col lg:flex-row xll:justify-between bg-[#FFFFFF] rounded-[10px]">
-                        <div>
+                    <div className="group rounded-[10px] border border-[#CBD5E1] overflow-hidden">
+                      <div className="portfolio-bgHover h-[420px] w-full cursor-pointer flex bg-[#FFFFFF] rounded-[10px]">
+                        <div className="w-1/2 h-full">
                           <Image
                             width={800}
-                            height={262}
-                            className="lg:w-[340px] 4xl:w-[332px] lg:h-[450px] 4xl:h-[420px] object-cover rounded-l-[10px]"
+                            height={500}
+                            className="w-full h-full object-cover rounded-l-[10px]"
                             src={portfolio?.image}
                             alt=""
                           />
                         </div>
-                        <div className="flex flex-col justify-center items-center p-3 md:py-0 xll:px-8 2xl:px-12 4xl:px-8">
-                          <div className="text-center h-[300px]">
-                            <h4 className="text-[14px] text-[#999999] pt-3 pb-3 md:pt-0 md:pb-6 portfolio-textHover ">
+                        <div className="w-1/2 h-[420px] flex flex-col justify-center items-center p-4">
+                          <div className="text-center">
+                            <h4 className="text-[14px] text-[#999999] pt-3 pb-3 md:pt-0 md:pb-6 portfolio-textHover">
                               {portfolio?.service_name[0]?.service_name}
                             </h4>
-                            <h3 className="text-[16px] w-[280px] font-bold font-Raleway text-[#333333] portfolio-textHover  ">
-                              {(() => {
-                                const originalHeading =
-                                  portfolio?.heading || "";
-                                const truncatedHeading = originalHeading.slice(
-                                  0,
-                                  145
-                                );
-                                const words = truncatedHeading.split(" ");
-                                const isHeadingTruncated =
-                                  originalHeading.length > 146;
-
-                                return (
-                                  <>
-                                    {words.map((word, index) => (
-                                      <React.Fragment key={index}>
-                                        {word}
-                                        {index === 3 ||
-                                        index === 9 ||
-                                        index === 12 ? (
-                                          <br />
-                                        ) : (
-                                          " "
-                                        )}
-                                      </React.Fragment>
-                                    ))}
-                                    {isHeadingTruncated && "..."}
-                                  </>
-                                );
-                              })()}
-                            </h3>
-                            <p className="w-[280px] text-center text-[14px] text-[#666666] py-3 portfolio-textHover pt-10">
-                              <span>{portfolio.text.slice(0, 195)}...</span>
-                            </p>
-                          </div>
-                          <div className="group flex justify-center items-center gap-2 text-[#FF693B] font-bold mt-10 portfolio-textHover pb-6 lg:pb-0 mt-14">
-                            <button className="text-[14px]">Read More</button>
-                            <span className="w-[19px] font-bold">
-                              <HiArrowSmallRight className="text-xl" />
-                            </span>
+                            <div className="text-[16px] px-[10%] w-full h-[75px] font-bold font-Raleway text-[#333333] portfolio-textHover line-clamp-3">
+                              {portfolio?.heading
+                                .split(" ")
+                                .slice(0, 12)
+                                .join(" ")}
+                              {portfolio?.heading.split(" ").length > 12
+                                ? "..."
+                                : ""}
+                            </div>
+                            <div>
+                              <p className="w-full px-[10%] flex justify-center text-[14px] text-[#666666] py-3 portfolio-textHover pt-7">
+                                <span>{portfolio.text.slice(0, 240)}...</span>
+                              </p>
+                              <div className="pt-0 group flex justify-center items-center gap-2 text-[#FF693B] font-bold portfolio-textHover pb-6 lg:pb-0">
+                                <button className="text-[14px]">
+                                  Read More
+                                </button>
+                                <span className="w-[19px] font-bold">
+                                  <HiArrowSmallRight className="text-xl" />
+                                </span>
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
