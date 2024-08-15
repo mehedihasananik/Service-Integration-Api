26277bf9483778c8c72964a4f7c1e753596904db
mehedihasananikoffcial <marshallanique@gmail.com>
2024-08-15 09:59:57 +0600

    after applying axios in banner

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import Container from "@/Components/Container/Container";
 import BannerItems from "@/Components/Utilites/BannerItems/BannerItems";
 import UserLoading from "@/Components/Utilites/UserLoading/UserLoading";
@@ -5,38 +6,23 @@ import { bannerApi } from "@/config/apis";
 import { Suspense } from "react";
 
 async function getBannerContent() {
-  const res = await fetch(`${bannerApi}`, {
-    next: { revalidate: 10 },
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+  try {
+    const res = await axios.get(bannerApi);
+    return res.data;
+  } catch (error) {
+    throw new Error("Failed to fetch banner data");
   }
-
-  const data = await res?.json();
-
-  if (!data || !data?.HomeBanner) {
-    throw new Error("Invalid banner data");
-  }
-
-  return data?.HomeBanner;
 }
 
 const Banner = async () => {
-  try {
-    const banner = await getBannerContent();
-    return (
-      <Container>
-        <Suspense fallback={<UserLoading />}>
-          <BannerItems banner={banner} />
-        </Suspense>
-      </Container>
-    );
-  } catch (error) {
-    // Handle errors here, e.g., display an error message
-    console.error("Error fetching banner:", error);
-    return <div>An error occurred while loading the banner.</div>;
-  }
+  const banner = await getBannerContent();
+  return (
+    <Container>
+      <Suspense fallback={<UserLoading />}>
+        <BannerItems banner={banner.HomeBanner} />
+      </Suspense>
+    </Container>
+  );
 };
 
 export default Banner;
