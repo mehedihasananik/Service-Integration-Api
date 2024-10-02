6124b3a3a35d7a486d2bac163dc1ea010d406b1f
mehedihasananikoffcial <marshallanique@gmail.com>
2024-10-02 16:16:33 +0600

    global error

--- src/Components/Utilites/Loading/Loading.jsx
+++ src/Components/Utilites/Loading/Loading.jsx
@@ -2,9 +2,9 @@ const Loading = () => {
   return (
     /* From Uiverse.io by Cybercom682 */
     /* From Uiverse.io by themrsami */
-    <div class="flex-col gap-4 w-full flex items-center justify-center">
-      <div class="w-20 h-20 border-4 border-transparent text-primary text-4xl animate-spin flex items-center justify-center border-t-secondary rounded-full">
-        <div class="w-16 h-16 border-4 border-transparent text-secondary text-2xl animate-spin flex items-center justify-center border-t-primary rounded-full"></div>
+    <div className="flex-col gap-4 w-full flex items-center justify-center">
+      <div className="w-20 h-20 border-4 border-transparent text-primary text-4xl animate-spin flex items-center justify-center border-t-secondary rounded-full">
+        <div className="w-16 h-16 border-4 border-transparent text-secondary text-2xl animate-spin flex items-center justify-center border-t-primary rounded-full"></div>
       </div>
       <span className="text-lg font-bold"> Loading...</span>
     </div>
--- src/app/global-error.js
+++ src/app/global-error.js
@@ -1,3 +1,5 @@
+"use client";
+
 export default function GlobalError({ error, reset }) {
   const handleRefreshAndNavigate = () => {
     window.location.href = "/"; // Refresh and navigate to "/home"
@@ -5,42 +7,15 @@ export default function GlobalError({ error, reset }) {
 
   return (
     <html>
-      <body
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          backgroundColor: "#f8d7da",
-          color: "#721c24",
-          fontFamily: "Arial, sans-serif",
-        }}
-      >
-        <div
-          style={{
-            textAlign: "center",
-            padding: "20px",
-            borderRadius: "8px",
-            backgroundColor: "#fff",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <h2 style={{ marginBottom: "20px" }}>Something went wrong!</h2>
-          <p style={{ marginBottom: "20px" }}>
-            Please try again later or return to the homepage.
-          </p>
+      <body className="flex items-center justify-center h-screen bg-red-100 text-red-800 font-sans">
+        <div className="text-center p-6 rounded-lg bg-white shadow-lg">
+          <h2 className="mb-4 text-2xl font-bold">Something went wrong!</h2>
+          <p className="mb-4">Please try again later</p>
           <button
             onClick={handleRefreshAndNavigate}
-            style={{
-              padding: "10px 20px",
-              border: "none",
-              borderRadius: "5px",
-              backgroundColor: "#007bff",
-              color: "#fff",
-              cursor: "pointer",
-            }}
+            className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 transition"
           >
-            Refresh
+            Please Refresh
           </button>
         </div>
       </body>
