abae1d2a0b69fff577c7ad498750c6b32e86bf29
mehedihasananikoffcial <marshallanique@gmail.com>
2024-10-02 16:26:28 +0600

    global error

--- src/app/global-error.js
+++ src/app/global-error.js
@@ -7,16 +7,18 @@ export default function GlobalError({ error, reset }) {
 
   return (
     <html>
-      <body className="flex items-center justify-center h-screen bg-red-100 text-red-800 font-sans">
-        <div className="text-center p-6 rounded-lg bg-white shadow-lg">
-          <h2 className="mb-4 text-2xl font-bold">Something went wrong!</h2>
-          <p className="mb-4">Please refresh the page from below:</p>
-          <button
-            onClick={handleHardRefreshAndNavigate}
-            className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 transition"
-          >
-            Refresh Now
-          </button>
+      <body className="">
+        <div className="w-full  h-screen flex items-center justify-center bg-red-200 text-red-800 font-sans">
+          <div className="text-center p-6 rounded-lg bg-white shadow-lg">
+            <h2 className="mb-4 text-2xl font-bold">Something went wrong!</h2>
+            <p className="mb-4">Please refresh the page from below:</p>
+            <button
+              onClick={handleHardRefreshAndNavigate}
+              className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 transition"
+            >
+              Refresh Now
+            </button>
+          </div>
         </div>
       </body>
     </html>
