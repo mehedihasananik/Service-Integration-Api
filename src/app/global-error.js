4d165fc400bf3052e8cea7bd06da962e8f9f6c68
mehedihasananikoffcial <marshallanique@gmail.com>
2024-10-02 16:05:25 +0600

    global error

--- src/app/global-error.js
+++ src/app/global-error.js
@@ -1,20 +1,49 @@
-"use client";
-
 export default function GlobalError({ error, reset }) {
   const handleRefreshAndNavigate = () => {
     window.location.href = "/"; // Refresh and navigate to "/home"
   };
 
   return (
-    <div className="text-center p-6 rounded-lg bg-white shadow-lg">
-      <h2 className="mb-4 text-2xl font-bold">Something went wrong!</h2>
-      <p className="mb-4">Please try again or return to the homepage.</p>
-      <button
-        onClick={handleRefreshAndNavigate}
-        className="px-4 py-2 text-white font-bold bg-secondary rounded hover:bg-primary transition"
+    <html>
+      <body
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          backgroundColor: "#f8d7da",
+          color: "#721c24",
+          fontFamily: "Arial, sans-serif",
+        }}
       >
-        Please Refresh
-      </button>
-    </div>
+        <div
+          style={{
+            textAlign: "center",
+            padding: "20px",
+            borderRadius: "8px",
+            backgroundColor: "#fff",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <h2 style={{ marginBottom: "20px" }}>Something went wrong!</h2>
+          <p style={{ marginBottom: "20px" }}>
+            Please try again later or return to the homepage.
+          </p>
+          <button
+            onClick={handleRefreshAndNavigate}
+            style={{
+              padding: "10px 20px",
+              border: "none",
+              borderRadius: "5px",
+              backgroundColor: "#007bff",
+              color: "#fff",
+              cursor: "pointer",
+            }}
+          >
+            Refresh
+          </button>
+        </div>
+      </body>
+    </html>
   );
 }
