ce29b634417a13b023ce183188c4136d38951efc
mehedihasananikoffcial <marshallanique@gmail.com>
2024-10-03 09:07:43 +0600

    updated global color

--- src/app/global-error.js
+++ src/app/global-error.js
@@ -8,13 +8,13 @@ export default function GlobalError({ error, reset }) {
   return (
     <html>
       <body className="">
-        <div className="w-full  h-screen flex items-center justify-center bg-red-200 text-red-800 font-sans">
+        <div className="w-full  h-screen flex items-center justify-center bg-gray-200 text-black font-sans">
           <div className="text-center p-6 rounded-lg bg-white shadow-lg">
             <h2 className="mb-4 text-2xl font-bold">Something went wrong!</h2>
             <p className="mb-4">Please refresh the page from below:</p>
             <button
               onClick={handleHardRefreshAndNavigate}
-              className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 transition"
+              className="px-4 py-2 text-white bg-[#123390] rounded hover:bg-primary transition"
             >
               Refresh Now
             </button>
