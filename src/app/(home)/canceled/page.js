5a767a8985d5ab05b8c2366bfdf501664a775b36
mehedihasananikoffcial <marshallanique@gmail.com>
2024-10-09 17:02:27 +0600

    after creating new  stripe on 9 oct

---
+++ src/app/(home)/canceled/page.js
@@ -0,0 +1,9 @@
+// app/canceled/page.js
+export default function CanceledPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-2xl font-bold">Payment Canceled</h1>
+      <p>Your transaction was canceled. Please try again.</p>
+    </div>
+  );
+}
---
+++ src/app/(home)/success/page.js
@@ -0,0 +1,12 @@
+// app/success/page.js
+export default function SuccessPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-2xl font-bold">Payment Successful!</h1>
+      <p>
+        Thank you for your purchase. Your transaction has been completed
+        successfully.
+      </p>
+    </div>
+  );
+}
