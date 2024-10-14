bca9022cb5fa5c0b2d95880b00a8aa9fc7e957a4
mehedihasananikoffcial <marshallanique@gmail.com>
2024-10-14 09:49:45 +0600

    updated with our team

---
+++ src/app/(home)/our-team/page.js
@@ -0,0 +1,19 @@
+import Container from "@/Components/Container/Container";
+import OurTeamContent from "@/Components/OurTeamContent/OurTeamContent";
+
+const OurTeam = () => {
+  return (
+    <div className=" bg-gray-100 py-8 mt-4">
+      <Container>
+        <div className="text-center">
+          <h1 className="text-[30px] md:text-[30px] lg:text-[48px] font-Raleway font-bold">
+            Our Team
+          </h1>
+        </div>
+        <OurTeamContent />
+      </Container>
+    </div>
+  );
+};
+
+export default OurTeam;
