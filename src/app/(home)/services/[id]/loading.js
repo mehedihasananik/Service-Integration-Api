61c9d1d08792bed3e4412125c6279fe5d66b95e0
mehedihasananikoffcial <marshallanique@gmail.com>
2024-10-29 09:17:45 +0600

    updated on 29 oct moring

---
+++ src/app/(home)/services/[id]/loading.js
@@ -0,0 +1,12 @@
+import UserLoading from "@/Components/Utilites/UserLoading/UserLoading";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="h-[50vh] flex justify-center items-center">
+      <UserLoading />
+    </div>
+  );
+};
+
+export default Loading;
